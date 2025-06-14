ed3e64ef4063444770684ab1148a80f4684d3402
Hunter121311-dev <damian.staron7@gmail.com>
2025-06-14 20:41:38 +0200

    tests results

--- backend/services/test.py
+++ backend/services/test.py
@@ -6,9 +6,7 @@ done = pd.read_csv("wyniki.csv")
 linki = pd.read_csv("C:/Users/damia/OneDrive/Dokumenty/GitHub/Youtube_Sponsorship_Detector/Linki.csv")
 for el in linki['link']:
     if el not in list(done['urls']):
-        done.loc[-1] = [el, '"'+str(generate_sponsorship_timestamps(find_url_core(el)))+'"']
-        done.index = done.index + 1
-        done = done.sort_index()
+        done = pd.concat([done, pd.DataFrame([{"urls":el, "timestamps": '"'+str(generate_sponsorship_timestamps(find_url_core(el)))+'"'}])], ignore_index=True)
         done.to_csv("wyniki.csv")
     else:
         continue
